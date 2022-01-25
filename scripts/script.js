2ecf3d5e38866941278fe51334de313617e8bef9
MasterrBo <seotitle@gmail.ru>
2022-01-25 20:40:01 +0300

    сверстать макет страницы

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+// const profileOpenPopupButton = document.querySelector('.edit-button')
+// const popup = document.querySelector('.popup')
+// const popupCloseButton = document.querySelector('.popup__close')
+
+
+// console.log(profileOpenPopupButton)
+
+
+
+function showMessage() {
+  document.getElementById('ID_popup').classList.add('popup_activ');
+}
+
+
+function closeMessage() {
+  document.getElementById('ID_popup').classList.remove('popup_activ');
+}
+
+
+// element.classList.add (name); name.
