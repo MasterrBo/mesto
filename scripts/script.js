5151e1b6ef8947d1389ce63ba192e539226e6425
MasterrBo <seotitle@gmail.ru>
2022-01-27 23:00:51 +0300

    написать js для поп-апа

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,50 @@
-// const profileOpenPopupButton = document.querySelector('.edit-button')
-// const popup = document.querySelector('.popup')
-// const popupCloseButton = document.querySelector('.popup__close')
-// console.log(profileOpenPopupButton)
+
+let profileAddButton = document.querySelector(".profile__add-button");
+let popupCloseButton = document.querySelector(".popup__close-button");
+let popup = document.querySelector(".popup");
+// let popupSaveButton = document.querySelector(".popup__close-button");
 
 function openPopup() {
-  document.getElementById('ID_popup').classList.add('popup_activ');
+  document.getElementById("ID_popup").classList.add("popup_activ");
 }
 
 function closePopup() {
-  document.getElementById('ID_popup').classList.remove('popup_activ');
+  document.getElementById("ID_popup").classList.remove("popup_activ");
 }
 
-profileButtonEdit.addEventListener('click', openPopup);
-profileButtonClose.addEventListener('click', closePopup);
-
-// function closePopup() {
-//   popap.classList.remove('popup__opened);
-// }
+profileAddButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__container')  // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector(".popup__container");
 
 // Находим поля формы в DOM
-let nameInput = document.getElementById('popup__name-field');  
-let jobInput = document.getElementById('popup__job-field'); 
-let profileName = document.querySelector('.popup__name-field');  // Воспользуйтесь инструментом .querySelector()
-let profileJob = document.querySelector('.popup__job-field'); // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.getElementById("name");
+let profileTitle = document.querySelector(".profile__title");
+let profilePost = document.querySelector(".profile__post");
+let jobInput = document.getElementById("job");
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profilePost.textContent = jobInput.value;
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
 
-let popupSave = document.querySelector('.popup__submit-button');
-popupSave.addEventListener("click", closePopup);
+formElement.addEventListener("submit", formSubmitHandler);
+popupSaveButton.addEventListener("click", closePopup);
+
+popup.addEventListener("click", function (event) {
+  if (event.defaultPrevented) {
+    closePopup();
+  }
+});
+
+
+
+
