49f439a5a6d78ab0032e99ec6d528095a58e281b
MasterrBo <seotitle@gmail.ru>
2022-02-12 15:05:50 +0300

    исправить ошибки после 3 проверки: хедер профайл футер для адаптива

--- scripts/index.js
+++ scripts/index.js
@@ -41,7 +41,41 @@ function formSubmitHandler(evt) {
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler);
 
+//новый код 
+let profileAddcardButton = document.querySelector(".profile__add-button");
+profileAddcardButton.addEventListener('click', openPopup);
+
+// Находим форму в DOM
+let formElement2 = document.querySelector(".popup__container_addcard");
+formElement2.addEventListener("submit", formSubmitHandler);
 
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
+// alert(initialCards); 
 
