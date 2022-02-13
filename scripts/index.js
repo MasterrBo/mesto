6edd824c6bea5c0540884b7aa89ecee3a7eb91e7
MasterrBo <seotitle@gmail.ru>
2022-02-13 17:22:46 +0300

    сделать разметку для второго попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Форма 1
 
 let profileAddButton = document.querySelector(".profile__edit-button");
 let popupCloseButton = document.querySelector(".popup__close-button");
@@ -33,3 +34,40 @@ function formSubmitHandler(evt) {
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
+
+// Форма 2
+
+let profileAddcardButton = document.querySelector(".profile__add-button");
+profileAddcardButton.addEventListener('click', openPopup2);
+let popup2 = document.querySelector(".popup2");
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+profileAddcardButton.addEventListener('click', openPopup2);
+popupCloseButton.addEventListener('click', closePopup);
+
+let formElement2 = document.querySelector(".popup__container_addcard");
+
+let nameInput2 = document.getElementById("photo-name"); 
+let linkInput = document.getElementById("link"); 
+let profileTitle2 = document.querySelector("photo-name");
+let profileLink = document.querySelector("link");
+
+function openPopup2() {
+  popup.classList.add("popup_opened");
+  nameInput2.value = profileTitle2.textContent;
+  linkInput.value = profileLink.textContent;
+}
+
+function formSubmitHandler2(evt) {
+  evt.preventDefault();
+  profileTitle2.textContent = nameInput2.value;
+  profileLink.textContent = linkInput.value;
+  console.log(nameInput2.value);
+  console.log(linkInput.value);
+  closePopup();
+}
+
+formElement2.addEventListener("submit", formSubmitHandler2);
